9d797ac910ad872f13284dff5c28d63039d90cb4
Panda-Young <1641140221@qq.com>
2025-04-10 03:52:43 +0800

    modify 3dc by c program

--- struct_test.c
+++ struct_test.c
@@ -29,10 +29,9 @@ typedef struct _TimedPos {
 #pragma pack()
 
 #pragma pack(1)
-typedef struct _AutomationPoints
-{
-	int16_t count;
-	TimedPos points[1];
+typedef struct _AutomationPoints {
+    int16_t count;
+    TimedPos points[1];
 } AutomationPoints;
 #pragma pack()
 
@@ -63,23 +62,12 @@ typedef struct _SoundField {
 } SoundField;
 #pragma pack(pop)
 
-typedef enum ChunkType {
+typedef enum {
     CHUNK_SPEAKER_POS = 1,
     CHUNK_SOUND_FIELD,
     CHUNK_AUDIO_AUTOMATION
-} ChunkType;
-
-typedef struct Parse3dcContent {
-    int set_done;
-    SoundField sound_field;
-    ObjectPos SpkPosData[12];
-    uint8_t speaker_count;
-    uint16_t point_count;
-    TimedPos timeSourcePosData[500];
-    int multi_position_number;
-    ObjectPos multiSourcePos[6];
-    ObjectPos multiListenerPos[6];
-} Parse3dcContent_t, *p_Parse3dcContent_t;
+} ChunkType_t,
+    *ChunkType_ptr;
 
 typedef struct {
     int id;
@@ -106,8 +94,68 @@ typedef struct {
 
 #include <stdio.h>
 
-int main() {
+void set_speaker_pos(ChimeConfig_t *chime_config, int id, float x, float y, float z)
+{
+    for (int i = 0; i < chime_config->speaker_count; i++) {
+        if (chime_config->SpkPosData[i].id == id) {
+            chime_config->SpkPosData[i].x = x;
+            chime_config->SpkPosData[i].y = y;
+            chime_config->SpkPosData[i].z = z;
+            break;
+        }
+    }
+}
+
+void check_info(ChimeConfig_t *chime_config)
+{
+    printf("file_header.formatId = %6s\n", chime_config->file_header.formatId);
+    printf("file_header.version = %d\n\n", chime_config->file_header.version);
+    printf("speaker_chunk_header.chunkType = %d\n", chime_config->speaker_chunk_header.chunkType);
+    printf("speaker_chunk_header.chunkDataSize = %d\n", chime_config->speaker_chunk_header.chunkDataSize);
+    printf("speaker_count = %d\n", chime_config->speaker_count);
+    for (int i = 0; i < chime_config->speaker_count; i++) {
+        printf("speaker[%d].id = %d x = %f y = %f z = %f\n",
+               i, chime_config->SpkPosData[i].id, chime_config->SpkPosData[i].x,
+               chime_config->SpkPosData[i].y, chime_config->SpkPosData[i].z);
+    }
+    printf("\n");
+    printf("sound_field_chunk_header.chunkType = %d\n", chime_config->sound_field_chunk_header.chunkType);
+    printf("sound_field_chunk_header.chunkDataSize = %d\n", chime_config->sound_field_chunk_header.chunkDataSize);
+    printf("sound_field.sourceId = %d\n", chime_config->sound_field.sourceId);
+    printf("sound_field.positionId = %d\n", chime_config->sound_field.positionId);
+    printf("sound_field.isLoop = %d\n", chime_config->sound_field.isLoop);
+    printf("sound_field.loopInterval = %d\n", chime_config->sound_field.loopInterval);
+    printf("sound_field.algorithmMode = %d\n", chime_config->sound_field.algorithmMode);
+    printf("sound_field.listenerPos.id = %d x = %f y = %f z = %f\n",
+           chime_config->sound_field.listenerPos.id, chime_config->sound_field.listenerPos.x,
+           chime_config->sound_field.listenerPos.y, chime_config->sound_field.listenerPos.z);
+    printf("sound_field.audioSourcePos.id = %d x = %f y = %f z = %f\n",
+           chime_config->sound_field.audioSourcePos.id, chime_config->sound_field.audioSourcePos.x,
+           chime_config->sound_field.audioSourcePos.y, chime_config->sound_field.audioSourcePos.z);
+    printf("sound_field.automationEnabled = %d\n", chime_config->sound_field.automationEnabled);
+}
+
+int main(int argc, char *argv[])
+{
     ChimeConfig_t chime_config = {0};
     printf("sizeof(ChimeConfig_t) = %lu\n", sizeof(ChimeConfig_t));
+    if (argc == 2) {
+        FILE *fp = fopen(argv[1], "rb+");
+        if (fp == NULL) {
+            printf("open file error\n");
+            return -1;
+        }
+        fread(&chime_config, sizeof(ChimeConfig_t), 1, fp);
+        printf("\n\noriginal data:\n");
+        check_info(&chime_config);
+        set_speaker_pos(&chime_config, 0, 2.4f, 0.0f, 2.7f);
+        set_speaker_pos(&chime_config, 1, -2.4f, 0.0f, 2.7f);
+        printf("\n\nmodified data:\n");
+        check_info(&chime_config);
+        fseek(fp, 0, SEEK_SET);
+        fwrite(&chime_config, sizeof(ChimeConfig_t), 1, fp);
+        fclose(fp);
+    }
+
     return 0;
 }
