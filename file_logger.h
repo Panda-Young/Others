f106fda79e924cfb77102de5b75d866a0717ec2c
pannyang <pandapan@aactechnologies.com>
2025-04-10 14:12:35 +0800

    update file logger

--- file_logger.h
+++ file_logger.h
@@ -6,14 +6,15 @@
  * Copyright (c) 2024 by Panda-Young, All Rights Reserved.
  **************************************************************************/
 
-#ifndef _LOGGER_H
-#define _LOGGER_H
+#ifndef __FILE_LOGGER_H
+#define __FILE_LOGGER_H
 
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <limits.h>
 #ifdef _WIN32
 #include <windows.h>
 #else
@@ -40,28 +41,32 @@ typedef enum {
     (strrchr(__FILE__, '/') ? (strrchr(__FILE__, '/') + 1) : (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__))
 #endif
 
-#define LOG_FILE_NAME "test.log"
+#define LOG_FILE_NAME "young.log"
 
 #ifdef _WIN32
-#define LOG(level, level_str, fmt, ...)                                                                        \
-    do {                                                                                                       \
-        if (log_level >= level) {                                                                              \
-            FILE *log_file = fopen(LOG_FILE_NAME, "a+");                                                       \
-            if (log_file) {                                                                                    \
-                SYSTEMTIME st;                                                                                 \
-                GetLocalTime(&st);                                                                             \
-                int pid = (int)GetCurrentProcessId();                                                          \
-                int tid = (int)GetCurrentThreadId();                                                           \
-                char _log_buf[256] = {0};                                                                      \
-                snprintf(_log_buf, sizeof(_log_buf), "%04d-%02d-%02d %02d:%02d:%02d.%03d [%d.%d] %s %s@%s:%d", \
-                         st.wYear, st.wMonth, st.wDay,                                                         \
-                         st.wHour, st.wMinute, st.wSecond, st.wMilliseconds,                                   \
-                         pid, tid, level_str, __FILENAME__, __FUNCTION__, __LINE__);                           \
-                fprintf(log_file, "%-96s" fmt "\n", _log_buf, ##__VA_ARGS__);                                  \
-                fflush(log_file);                                                                              \
-                fclose(log_file);                                                                              \
-            }                                                                                                  \
-        }                                                                                                      \
+#define LOG(level, level_str, fmt, ...)                                                                            \
+    do {                                                                                                           \
+        if (log_level >= level) {                                                                                  \
+            char log_file_path[MAX_PATH] = {0};                                                                    \
+            if (GetTempPath(MAX_PATH, log_file_path) > 0) {                                                        \
+                strcat(log_file_path, LOG_FILE_NAME);                                                              \
+                FILE *log_file = fopen(log_file_path, "a+");                                                       \
+                if (log_file) {                                                                                    \
+                    SYSTEMTIME st;                                                                                 \
+                    GetLocalTime(&st);                                                                             \
+                    int pid = (int)GetCurrentProcessId();                                                          \
+                    int tid = (int)GetCurrentThreadId();                                                           \
+                    char _log_buf[256] = {0};                                                                      \
+                    snprintf(_log_buf, sizeof(_log_buf), "%04d-%02d-%02d %02d:%02d:%02d.%03d [%d.%d] %s %s@%s:%d", \
+                             st.wYear, st.wMonth, st.wDay,                                                         \
+                             st.wHour, st.wMinute, st.wSecond, st.wMilliseconds,                                   \
+                             pid, tid, level_str, __FILENAME__, __FUNCTION__, __LINE__);                           \
+                    fprintf(log_file, "%-96s" fmt "\n", _log_buf, ##__VA_ARGS__);                                  \
+                    fflush(log_file);                                                                              \
+                    fclose(log_file);                                                                              \
+                }                                                                                                  \
+            }                                                                                                      \
+        }                                                                                                          \
     } while (0)
 #else
 #define LOG(level, level_str, fmt, ...)                                                                         \
@@ -93,4 +98,4 @@ typedef enum {
 #define LOGW(fmt, ...) LOG(LOG_WARNING, "WARN ", fmt, ##__VA_ARGS__)
 #define LOGE(fmt, ...) LOG(LOG_ERROR, "ERROR", fmt, ##__VA_ARGS__)
 
-#endif // _LOGGER_H
+#endif // __FILE_LOGGER_H
